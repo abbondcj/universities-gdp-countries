5fcb4cce2cfe73ca70be60b1d7f0c782e3383a0b
Chris Abbondandelo <abbondcj@gmail.com>
2022-01-16 14:36:36 -0600

    testing basic functionality of the api, also added heading/input/button

---
+++ README.md
@@ -0,0 +1,1 @@
+Number of universities & GDP by country
---
+++ main.js
@@ -0,0 +1,11 @@
+// $.getJSON("http://universities.hipolabs.com/search?", 
+// function (data) {
+//     let usaCount = 0;
+//     console.log(usaCount);
+//     for (i=0; i<data.length; i++) {
+//         if (data[i].country == "United States") {
+//             usaCount += 1
+//         };
+//     };
+//     console.log(usaCount);
+// });
